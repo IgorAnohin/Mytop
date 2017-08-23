951c20528945c7dc3122f7086c7efdd6be60e075
Igor Anohin <igor.anohin@emlid.com>
2017-08-23 14:27:22 +0300

    complete

--- sort.c
+++ sort.c
@@ -53,22 +53,6 @@ enum {
         } while (--__size > 0);                                                      \
     } while (0)
  
-//typedef int (*__compar_d_fn_t) (const void *, const void *, void *);
-
-#define MAX_THRESH 4
- 
-/* Stack node declarations used to store unfulfilled partition obligations. */
-typedef struct
-  {
-    char *lo;
-    char *hi;
-  } stack_node;
- 
-
-#define STACK_SIZE        (CHAR_BIT * sizeof(size_t))
-#define PUSH(low, high)        ((void) ((top->lo = (low)), (top->hi = (high)), ++top))
-#define POP(low, high)        ((void) (--top, (low = top->lo), (high = top->hi)))
-
 int sorting(const void * x,const void * x1, void * arg) {
     data_top * P = (data_top *)x;
     data_top * P1 = (data_top *)x1;
@@ -158,148 +142,51 @@ int sorting(const void * x,const void * x1, void * arg) {
 
 }
 
-//    qsort(solution,count_proc, sizeof(parametrs),sorting);
 
 
-
-void quicksort (void *const pbase, size_t total_elems, size_t size,
-            __compar_d_fn_t cmp, void *arg)
-
+static void fastsort(char* low, char* hight,size_t size, __compar_d_fn_t cmp, void *arg)
 {
-  char *base_ptr = (char *) pbase;
- 
-  const size_t max_thresh = MAX_THRESH * size;
- 
- 
-  if (total_elems > MAX_THRESH)
+
+    if ( low < hight  )
     {
-      char *lo = base_ptr;
-      char *hi = &lo[size * (total_elems - 1)];
-      stack_node stack[STACK_SIZE];
-      stack_node *top = stack;
- 
-      PUSH (NULL, NULL);
- 
-      while (stack < top)
-        {
-          char *left_ptr;
-          char *right_ptr;
- 
-          //small element
-          char *mid = lo + size * ((hi - lo) / size >> 1);
- 
-          if ( (*cmp) ((void *) mid, (void *) lo, arg) < 0)
-            SWAP (mid, lo, size);
-          if ( (*cmp) ((void *) hi, (void *) mid, arg) < 0)
-            SWAP (mid, hi, size);
-          
- 
-          left_ptr  = lo + size;
-          right_ptr = hi - size;
- 
+            char *lo = low; char *hi = hight;
 
-          do
-            {
-              while ((*cmp) ((void *) left_ptr, (void *) mid, arg) < 0)
-                left_ptr += size;
- 
-              while ((*cmp) ((void *) mid, (void *) right_ptr, arg) < 0)
-                right_ptr -= size;
- 
-              if (left_ptr < right_ptr)
-                {
-                  SWAP (left_ptr, right_ptr, size);
-                  if (mid == left_ptr)
-                    mid = right_ptr;
-                  else if (mid == right_ptr)
-                    mid = left_ptr;
-                  left_ptr += size;
-                  right_ptr -= size;
+            char* mid = lo + size * ( (hi - lo) / size >> 1 );
+
+
+            do {
+                while ( (*cmp) ( (void*)mid, (void*)lo, arg) > 0)
+                    lo += size;
+
+
+                while ( (*cmp) ( (void*)hi,(void*)mid, arg) > 0)
+                    hi -= size;
+
+                if (lo <= hi) {
+                    SWAP (lo,hi,size);
+                    lo +=size;
+                    hi -=size;
+
                 }
-              else if (left_ptr == right_ptr)
-                {
-                  left_ptr += size;
-                  right_ptr -= size;
-                  break;
-                }
-            }
-          while (left_ptr <= right_ptr);
- 
- 
-          if ((size_t) (right_ptr - lo) <= max_thresh)
-            {
-              if ((size_t) (hi - left_ptr) <= max_thresh)
-                /* Ignore both small partitions. */
-                POP (lo, hi);
-              else
-                /* Ignore small left partition. */
-                lo = left_ptr;
-            }
-          else if ((size_t) (hi - left_ptr) <= max_thresh)
-            /* Ignore small right partition. */
-            hi = right_ptr;
-          else if ((right_ptr - lo) > (hi - left_ptr))
-            {
-              /* Push larger left partition indices. */
-              PUSH (lo, right_ptr);
-              lo = left_ptr;
-            }
-          else
-            {
-              /* Push larger right partition indices. */
-              PUSH (left_ptr, hi);
-              hi = right_ptr;
-            }
-        }
+
+            } while (lo <= hi);
+
+
+            if (hi>low) fastsort(low, hi, size, cmp, arg);
+            if (hight>lo) fastsort(lo, hight, size, cmp, arg);
     }
- 
-
- 
-#define min(x, y) ((x) < (y) ? (x) : (y))
- 
-
-    char *const end_ptr = &base_ptr[size * (total_elems - 1)];
-    char *tmp_ptr = base_ptr;
-    char *thresh = min(end_ptr, base_ptr + max_thresh);
-    char *run_ptr;
- 
-    
-    //smallest element
-    for (run_ptr = tmp_ptr + size; run_ptr <= thresh; run_ptr += size)
-      if ((*cmp) ((void *) run_ptr, (void *) tmp_ptr, arg) < 0)
-        tmp_ptr = run_ptr;
- 
-    if (tmp_ptr != base_ptr)
-      SWAP (tmp_ptr, base_ptr, size);
- 
-
-    //insert sort
-    run_ptr = base_ptr + size;
-    while ((run_ptr += size) <= end_ptr)
-      {
-        tmp_ptr = run_ptr - size;
-        while ((*cmp) ((void *) run_ptr, (void *) tmp_ptr, arg) < 0)
-          tmp_ptr -= size;
- 
-        tmp_ptr += size;
-        if (tmp_ptr != run_ptr)
-          {
-            char *trav;
- 
-            trav = run_ptr + size;
-            while (--trav >= run_ptr)
-              {
-                char c = *trav;
-                char *hi, *lo;
- 
-                for (hi = lo = trav; (lo -= size) >= tmp_ptr; hi = lo)
-                  *hi = *lo;
-                *hi = c;
-              }
-          }
-      }
-
 }
+
+
+void quicksort(void *const pbase, size_t total_elems, size_t size,
+                   __compar_d_fn_t cmp, void *arg)
+{
+    char * low = (char*)pbase;
+    char * hight = &low[size * (total_elems - 1)];
+    fastsort(low,hight,size,cmp,arg);
+}
+
+
 
 
 
